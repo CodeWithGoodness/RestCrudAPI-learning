c9bdc60a535166e707a870db20228288144e2fea
Goodness Okoro <ceciliaokoro2@gmail.com>
2022-08-04 22:12:48 +0100

    first commit

--- src/main/java/com/goodness/springweb/ProductrestapiApplication.java
+++ src/main/java/com/goodness/springweb/ProductrestapiApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.goodness.springweb, com.goodness.springweb.controllers,"
-// + "com.goodness.springweb.entities, com.goodness.springweb.repos" )
+
 public class ProductrestapiApplication {
 
 	public static void main(String[] args) {
